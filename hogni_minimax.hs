8cd235e12d2ca7669a8f066041d73439e424befa
hogni89 <joensen.hogni@gmail.com>
2015-01-08 02:20:08 +0000

    Delete hogni_minimax.hs

--- hogni_minimax.hs
+++
@@ -1,4 +0,0 @@
-minimax :: Game g => g -> GTree g -> (Maybe (Move g), Value)
-minimax g (Tree (p, s) []) = (Nothing, (value g p s))
-minimax g (Tree (True, s) v) = maximumSnd [(Just m, snd $ minimax g ch) | (m,ch) <- v]
-minimax g (Tree (False, s) v) = minimumSnd [(Just m, snd $ minimax g ch) | (m,ch) <- v]
